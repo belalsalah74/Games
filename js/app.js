ba69cd33e611ae455abda19a30007b81df2f6fb9
belalsalah <belalsalah74@gmailcom>
2024-12-18 15:21:34 +0200

    fix url

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ import Ui from "./ui.js";
 export default class App {
   static #request = new Request();
   static #ui = new Ui();
-  static #baseUrl = "https://www.freetogame.com/api/";
+  static #baseUrl = "https://free-to-play-games-database.p.rapidapi.com/api/";
   static #options = {
     method: "GET",
     headers: {
