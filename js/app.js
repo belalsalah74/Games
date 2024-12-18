96a65c96d81283e268b9b9b125d79165e2a4953d
belalsalah <belalsalah74@gmailcom>
2024-12-18 15:28:26 +0200

    fix modal

--- js/app.js
+++ js/app.js
@@ -60,6 +60,7 @@ export default class App {
           );
         }
         App.#ui.displayAll(data);
+        this.#handleDetails();
         document.querySelector(".navbar-collapse").classList.remove("show");
       })
     );
